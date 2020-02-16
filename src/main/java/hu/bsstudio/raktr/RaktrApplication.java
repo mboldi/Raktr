5d13bc1682b72bd495ff156a2d128dbfca797f45
Márta Boldizsár <marta.boldi13@gmail.com>
2020-02-16 22:02:10 +0100

    initial commit

---
+++ src/main/java/hu/bsstudio/raktr/RaktrApplication.java
@@ -0,0 +1,13 @@
+package hu.bsstudio.raktr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaktrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaktrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/bsstudio/raktr/RaktrApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.bsstudio.raktr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaktrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

698ccdb3b19063edc6eb72f4b428e0dd9873757b
Márta Boldizsár <marta.boldi13@gmail.com>
2020-03-19 16:28:55 +0100

    checkstyle formatting

--- src/test/java/hu/bsstudio/raktr/RaktrApplicationTests.java
+++ src/test/java/hu/bsstudio/raktr/RaktrApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RaktrApplicationTests {
-  /*
-	@Test
-	void contextLoads() {
-	}*/
+/*
+@Test
+void contextLoads() {
+}*/
 }

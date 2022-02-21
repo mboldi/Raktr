ebeaff8950816fdd99748742b6898b35aa746bf2
Márta Boldizsár <marta.boldi13@gmail.com>
2022-02-21 17:36:34 +0100

    added option to add comment to ticket in backend

--- backend/src/main/java/hu/bsstudio/raktr/service/TicketService.java
+++ backend/src/main/java/hu/bsstudio/raktr/service/TicketService.java
@@ -1,13 +1,13 @@
 package hu.bsstudio.raktr.service;
 
+import hu.bsstudio.raktr.model.Comment;
 import hu.bsstudio.raktr.model.Ticket;
 import hu.bsstudio.raktr.repository.TicketRepository;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
 
 @Service
 @Slf4j
@@ -27,8 +27,9 @@ public class TicketService {
     public Optional<Ticket> addTicket(final Ticket newTicket) {
         var foundTicket = newTicket.getId() == null ? Optional.empty() : ticketRepository.findById(newTicket.getId());
 
-        if (foundTicket.isPresent())
+        if (foundTicket.isPresent()) {
             return Optional.empty();
+        }
 
         Ticket saved = ticketRepository.save(newTicket);
 
@@ -40,8 +41,9 @@ public class TicketService {
     public Optional<Ticket> updateTicket(final Ticket ticketToUpdate) {
         var foundTicket = ticketRepository.findById(ticketToUpdate.getId());
 
-        if (foundTicket.isEmpty())
+        if (foundTicket.isEmpty()) {
             return Optional.empty();
+        }
 
         foundTicket.get().setStatus(ticketToUpdate.getStatus());
         foundTicket.get().setSeverity(ticketToUpdate.getSeverity());
@@ -57,11 +59,29 @@ public class TicketService {
     public List<Ticket> getTicketsOfScannable(final Long scannableId) {
         List<Ticket> ticketsOfScannable = ticketRepository.findByScannableId(scannableId);
 
-        if (ticketsOfScannable.isEmpty())
+        if (ticketsOfScannable.isEmpty()) {
             log.info("No tickets found for scannable with id {}", scannableId);
-        else
+        } else {
             log.info("Tickets found for scannable: {}", ticketsOfScannable);
+        }
 
         return ticketsOfScannable;
+    }
+
+    public Optional<Ticket> addCommentToTicket(final Long ticketId, final Comment comment) {
+        var ticketToUpdate = ticketRepository.findById(ticketId);
+
+        if (ticketToUpdate.isEmpty()) {
+            log.warn("Ticket not found to add comment to with id: {}", ticketId);
+            return Optional.empty();
+        }
+
+        ticketToUpdate.get().addComment(comment);
+
+        ticketRepository.save(ticketToUpdate.get());
+
+        log.info("Saved ticket with comment: {}", ticketToUpdate.get());
+
+        return ticketToUpdate;
     }
 }

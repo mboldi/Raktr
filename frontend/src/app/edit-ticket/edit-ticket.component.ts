f3c04db645320d3f0c7758043699900099d9a5db
Márta Boldizsár <marta.boldi13@gmail.com>
2022-02-21 17:11:46 +0100

    added ticket editing page & ticket service

---
+++ frontend/src/app/edit-ticket/edit-ticket.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+
+@Component({
+    selector: 'app-edit-ticket',
+    templateUrl: './edit-ticket.component.html',
+    styleUrls: ['./edit-ticket.component.css'],
+    encapsulation: ViewEncapsulation.None,
+})
+export class EditTicketComponent implements OnInit {
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+}

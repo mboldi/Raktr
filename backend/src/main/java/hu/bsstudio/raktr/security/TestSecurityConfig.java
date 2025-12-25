3494491953e5c5a083749a8fdbcdaad0d886ec90
Olivér Kecskeméty <kecskemety.oliver@gmail.com>
2025-12-26 00:26:28 +0100

    Add CORS settings to TestSecurityConfig

--- backend/src/main/java/hu/bsstudio/raktr/security/TestSecurityConfig.java
+++ backend/src/main/java/hu/bsstudio/raktr/security/TestSecurityConfig.java
@@ -3,6 +3,7 @@ package hu.bsstudio.raktr.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,6 +15,7 @@ public class TestSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
 

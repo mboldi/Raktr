bf8ee1d10a03907c05b51f32db180f526968bc03
Olivér Kecskeméty <kecskemety.oliver@gmail.com>
2025-12-30 03:46:51 +0100

    Forgot to remove this

--- backend/src/main/java/hu/bsstudio/raktr/controller/DeviceController.java
+++ backend/src/main/java/hu/bsstudio/raktr/controller/DeviceController.java
@@ -5,10 +5,16 @@ import hu.bsstudio.raktr.service.DeviceService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Objects;
@@ -17,7 +23,6 @@ import java.util.stream.Collectors;
 @RestController
 @Slf4j
 @RequestMapping("/api/device")
-@PropertySource("classpath:userInfo.properties")
 @RequiredArgsConstructor
 public class DeviceController {
 

52f5c49e9e29ad1611ab1b071ad1f83367345aa8
Olivér Kecskeméty <kecskemety.oliver@gmail.com>
2026-01-09 01:56:08 +0100

    Remove unused

--- backend/src/main/java/hu/bsstudio/raktr/dal/repository/OwnerRepository.java
+++ backend/src/main/java/hu/bsstudio/raktr/dal/repository/OwnerRepository.java
@@ -3,12 +3,8 @@ package hu.bsstudio.raktr.dal.repository;
 import hu.bsstudio.raktr.dal.entity.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface OwnerRepository extends JpaRepository<Owner, Integer> {
 
     boolean existsByName(String name);
-
-    Optional<Owner> findByName(String name);
 
 }

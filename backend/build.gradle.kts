79fce5f2c00428409a2f48f6349d26700e24ccdc
Olivér Kecskeméty <kecskemety.oliver@gmail.com>
2026-03-15 02:50:15 +0100

    Upgrade dependencies

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     java
     jacoco
-    id("io.freefair.lombok") version "9.1.0"
+    id("io.freefair.lombok") version "9.2.0"
     id("io.spring.dependency-management") version "1.1.7"
-    id("org.springframework.boot") version "4.0.1"
+    id("org.springframework.boot") version "4.0.3"
 }
 
 group = "hu.bsstudio"
@@ -28,7 +28,7 @@ repositories {
 dependencies {
     implementation("com.itextpdf:itext7-core:9.5.0")
     implementation("org.mapstruct:mapstruct:1.6.3")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.1")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2")
     implementation("org.springframework.boot:spring-boot-configuration-processor")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-cache")
@@ -41,8 +41,8 @@ dependencies {
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
-    runtimeOnly("org.flywaydb:flyway-database-postgresql:11.20.1")
-    runtimeOnly("org.postgresql:postgresql:42.7.8")
+    runtimeOnly("org.flywaydb:flyway-database-postgresql:12.1.0")
+    runtimeOnly("org.postgresql:postgresql:42.7.10")
 
     annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
@@ -53,7 +53,7 @@ dependencies {
     testImplementation("org.testcontainers:testcontainers-postgresql:2.0.3")
     testImplementation("io.rest-assured:rest-assured:6.0.0")
     testImplementation("org.wiremock:wiremock-standalone:3.13.2")
-    testImplementation("com.nimbusds:nimbus-jose-jwt:10.7")
+    testImplementation("com.nimbusds:nimbus-jose-jwt:10.8")
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }

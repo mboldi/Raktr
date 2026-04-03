f72675c4c9e3426b2d064cd7d69cc70c96e39292
Boldizsár Márta <marta.boldi13@gmail.com>
2026-04-04 01:02:50 +0200

    start of design of overview component

--- frontend/src/app/layouts/main-layout/overview/overview.component.ts
+++ frontend/src/app/layouts/main-layout/overview/overview.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import {MatCard, MatCardContent, MatCardHeader} from '@angular/material/card';
+import {MatIcon} from '@angular/material/icon';
+import {MatFormField, MatInput, MatLabel, MatSuffix} from '@angular/material/input';
+import {MatFabButton, MatIconButton} from '@angular/material/button';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-overview',
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatIcon,
+    MatCardContent,
+    MatFormField,
+    MatLabel,
+    MatInput,
+    MatIconButton,
+    MatSuffix,
+    ReactiveFormsModule,
+    MatFabButton
+  ],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.scss',
 })
 export class OverviewComponent {
+  protected deviceSearchFormControl: FormControl = new FormControl();
 
+  protected searchDevice() {
+    console.log(this.deviceSearchFormControl.value);
+    this.deviceSearchFormControl.setValue("");
+  }
+
+  protected addRent() {
+
+  }
 }

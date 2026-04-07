10cb721a2fecb2f75034e64ed5fc354c30c859ba
Boldizsár Márta <marta.boldi13@gmail.com>
2026-04-07 21:05:14 +0200

    refactored styling of overview component

--- frontend/src/app/layouts/main-layout/main-layout.component.ts
+++ frontend/src/app/layouts/main-layout/main-layout.component.ts
@@ -1,14 +1,11 @@
 import {Component} from '@angular/core';
 import {SidebarComponent} from '../../components/sidebar/sidebar.component';
-import {MatButton} from '@angular/material/button';
-import {CategoryService} from '../../services/category.service';
 import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-main-layout',
   imports: [
     SidebarComponent,
-    MatButton,
     RouterOutlet
   ],
   templateUrl: './main-layout.component.html',
@@ -16,14 +13,7 @@ import {RouterOutlet} from '@angular/router';
 })
 export class MainLayoutComponent {
 
-  constructor(private categoryService: CategoryService) {
-  }
-
-  protected getCategories() {
-
-    this.categoryService.getCategories().subscribe(categories => {
-      console.log(categories);
-    })
+  constructor() {
   }
 
 

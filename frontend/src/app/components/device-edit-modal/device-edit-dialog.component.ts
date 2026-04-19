d0168eede4474b73ed8bca5e66a39ba8b57c75c3
Márta Boldizsár <marta.boldi13@gmail.com>
2026-04-19 16:29:08 +0200

    all fields present for device editing dialog

--- frontend/src/app/components/device-edit-modal/device-edit-dialog.component.ts
+++ frontend/src/app/components/device-edit-modal/device-edit-dialog.component.ts
@@ -2,7 +2,6 @@ import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {MatDialogActions, MatDialogClose, MatDialogContent, MatDialogTitle} from '@angular/material/dialog';
 import {MatButton} from '@angular/material/button';
 import {OwnerService} from '../../services/owner.service';
-import {Owner} from '../../model/owner/owner';
 import {MatFormField, MatInput, MatInputModule, MatLabel} from '@angular/material/input';
 import {MatCheckbox} from '@angular/material/checkbox';
 import {CategoryDetails} from '../../model/category/categoryDetails';
@@ -16,6 +15,8 @@ import {LocationService} from '../../services/location.service';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
+import {OwnerDetailsDto} from "../../model/owner/ownerDetailsDto";
+import {MatRadioButton, MatRadioGroup} from "@angular/material/radio";
 
 @Component({
   selector: 'app-device-edit-modal',
@@ -37,23 +38,27 @@ import {MatIconModule} from '@angular/material/icon';
     MatFormFieldModule,
     MatInputModule,
     MatDatepickerModule,
-    MatIconModule
+    MatIconModule,
+    MatRadioGroup,
+    MatRadioButton
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './device-edit-dialog.component.html',
   styleUrl: './device-edit-dialog.component.scss',
 })
 export class DeviceEditDialogComponent implements OnInit {
-  protected owners: Owner[] = [];
-
   protected categories: CategoryDetails[] = [];
   protected filteredCategories: Observable<CategoryDetails[]>;
 
   protected locations: LocationDetails[] = [];
   protected filteredLocations: Observable<LocationDetails[]>;
 
+  protected owners: OwnerDetailsDto[] = [];
+  protected filteredOwners: Observable<OwnerDetailsDto[]>;
+
   protected categoryControl = new FormControl();
   protected locationControl = new FormControl();
+  protected ownerControl = new FormControl();
 
   constructor(private ownerService: OwnerService,
               private categoryService: CategoryService,
@@ -66,7 +71,12 @@ export class DeviceEditDialogComponent implements OnInit {
     this.filteredLocations = this.locationControl.valueChanges.pipe(
       startWith(''),
       map(value => this.filterLocations(value || ''))
-    )
+    );
+
+    this.filteredOwners = this.ownerControl.valueChanges.pipe(
+        startWith(''),
+        map(value => this.filterOwners(value || ''))
+    );
   }
 
   ngOnInit() {
@@ -93,6 +103,12 @@ export class DeviceEditDialogComponent implements OnInit {
     const filter = value.toLowerCase();
 
     return this.locations.filter(location => location.name.toLowerCase().includes(filter)).slice(0, 3);
+  }
+
+  private filterOwners(value: string) {
+    const filter = value.toLowerCase();
+
+    return this.owners.filter(owner => owner.name.toLowerCase().includes(filter)).slice(0, 3);
   }
 
 }

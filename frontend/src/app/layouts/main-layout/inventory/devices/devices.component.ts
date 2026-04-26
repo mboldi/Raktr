12e9691cbf1e91587c955a71ee9d8b0361e996ac
Boldizsár Márta <marta.boldi13@gmail.com>
2026-04-26 20:03:06 +0200

    added header box to device list view

--- frontend/src/app/layouts/main-layout/inventory/devices/devices.component.ts
+++ frontend/src/app/layouts/main-layout/inventory/devices/devices.component.ts
@@ -1,85 +1,82 @@
 import {Component, OnInit} from '@angular/core';
 import {
-    MatCell,
-    MatCellDef,
-    MatColumnDef,
-    MatHeaderCell,
-    MatHeaderCellDef,
-    MatHeaderRow,
-    MatHeaderRowDef,
-    MatRow,
-    MatRowDef,
-    MatTable
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow,
+  MatHeaderRowDef,
+  MatRow,
+  MatRowDef,
+  MatTable
 } from "@angular/material/table";
 import {MatFormField, MatInput, MatLabel, MatSuffix} from "@angular/material/input";
 import {DeviceService} from "../../../../services/device.service";
 import {DeviceDetails} from "../../../../model/scannable/device/DeviceDetails";
 import {MatPaginator} from "@angular/material/paginator";
-import {OwnerDetailsDto} from "../../../../model/owner/ownerDetailsDto";
-import {UserDetails} from "../../../../model/user/userDetails";
-import {DeviceStatus} from "../../../../model/scannable/device/deviceStatus";
 import {DecimalPipe} from "@angular/common";
 import {MatSortModule, Sort} from "@angular/material/sort";
 import {FormControl, FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatIcon} from "@angular/material/icon";
-import {MatIconButton} from "@angular/material/button";
+import {MatFabButton, MatIconButton} from "@angular/material/button";
+import {MatCard} from '@angular/material/card';
 
 const ALL_COLUMNS: string[] = ['name', 'assetTag', 'maker', 'model', 'quantity', 'category', 'location', 'weight'];
 const REDUCED_COLUMNS: string[] = ['name', 'assetTag', 'maker', 'model', 'quantity'];
 
 @Component({
-    selector: 'app-devices',
-    imports: [
-        MatTable,
-        MatFormField,
-        MatLabel,
-        MatInput,
-        MatColumnDef,
-        MatHeaderCell,
-        MatCell,
-        MatCellDef,
-        MatHeaderCellDef,
-        MatPaginator,
-        MatHeaderRow,
-        MatRow,
-        MatHeaderRowDef,
-        MatRowDef,
-        DecimalPipe,
-        MatSortModule,
-        FormsModule,
-        MatIcon,
-        MatIconButton,
-        MatSuffix,
-        ReactiveFormsModule
-    ],
-    templateUrl: './devices.component.html',
-    styleUrl: './devices.component.scss',
+  selector: 'app-devices',
+  imports: [
+    MatTable,
+    MatFormField,
+    MatLabel,
+    MatInput,
+    MatColumnDef,
+    MatHeaderCell,
+    MatCell,
+    MatCellDef,
+    MatHeaderCellDef,
+    MatPaginator,
+    MatHeaderRow,
+    MatRow,
+    MatHeaderRowDef,
+    MatRowDef,
+    DecimalPipe,
+    MatSortModule,
+    FormsModule,
+    MatIcon,
+    MatIconButton,
+    MatSuffix,
+    ReactiveFormsModule,
+    MatCard,
+    MatFabButton
+  ],
+  templateUrl: './devices.component.html',
+  styleUrl: './devices.component.scss',
 })
 export class DevicesComponent implements OnInit {
 
-    protected deviceSearchFormControl = new FormControl();
+  protected deviceSearchFormControl = new FormControl();
 
-    protected displayedColumns = ALL_COLUMNS;
+  protected displayedColumns = ALL_COLUMNS;
 
-    protected devices: DeviceDetails[] = [
-        new DeviceDetails(1, "B-CAM-01", "0000001", "320-1", 8654, false, false, "Videó", "Stúdiótér", new OwnerDetailsDto(1, "SVIE", true, new Date(), new UserDetails("1qda", "mboldi", "Márta", "Boldizsár", "ifjB", "", []), new Date(), new UserDetails("1qda", "mboldi", "Márta", "Boldizsár", "ifjB", "", [])), new Date(), new UserDetails("1qda", "mboldi", "Márta", "Boldizsár", "ifjB", "", []), new Date(), new UserDetails("1qda", "mboldi", "Márta", "Boldizsár", "ifjB", "", []), "Sony", "PMW-320", "000000111", 85000, DeviceStatus.GOOD, 1, "KPR", new Date(), new Date(), ""),
-        new DeviceDetails(1, "B-CAM-02", "0000002", "500-1", 8654, false, false, "Videó", "Stúdiótér", new OwnerDetailsDto(1, "SVIE", true, new Date(), new UserDetails("1qda", "mboldi", "Márta", "Boldizsár", "ifjB", "", []), new Date(), new UserDetails("1qda", "mboldi", "Márta", "Boldizsár", "ifjB", "", [])), new Date(), new UserDetails("1qda", "mboldi", "Márta", "Boldizsár", "ifjB", "", []), new Date(), new UserDetails("1qda", "mboldi", "Márta", "Boldizsár", "ifjB", "", []), "Sony", "PMW-320", "000000111", 85000, DeviceStatus.GOOD, 1, "KPR", new Date(), new Date(), "")
-    ];
+  protected devices: DeviceDetails[] = [];
 
-    constructor(
-        private deviceService: DeviceService,) {
-    }
+  constructor(
+    private deviceService: DeviceService,) {
+  }
 
-    ngOnInit() {
-        /*this.deviceService.getDevices().subscribe(devices => {
-          this.devices = devices;
-        });*/
-    }
+  ngOnInit() {
+    this.deviceService.getDevices().subscribe(devices => {
+      this.devices = devices;
+    });
+  }
 
-    protected applyFilter($event: KeyboardEvent) {
-    }
+  protected applyFilter($event: KeyboardEvent) {
+  }
 
-    protected announceSortChange($event: Sort) {
-        console.log($event);
-    }
+  protected announceSortChange($event: Sort) {
+    console.log($event);
+  }
 }

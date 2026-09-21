4a08cc128bb97b80e74ecbaa0a26fc7428301fcb
Boldizsár Márta <marta.boldi13@gmail.com>
2026-09-21 14:05:24 +0200

    prettier fix

--- frontend/src/app/layouts/main-layout/settings/settings.component.ts
+++ frontend/src/app/layouts/main-layout/settings/settings.component.ts
@@ -13,7 +13,7 @@ import { UserDetails } from '../../../model/user/userDetails';
 import { UserUpdateDto } from '../../../model/user/userUpdateDto';
 import { ConfigUpdateDto } from '../../../model/config/configUpdateDto';
 import { environment } from '../../../../environments/environment';
-import {MatBadge} from '@angular/material/badge';
+import { MatBadge } from '@angular/material/badge';
 
 @Component({
   selector: 'app-settings',
